25925bd1cccff04e9dbf117e8c0e509b314e4cff
Ian Thompson <ianthompson@nicelion.com>
2023-02-17 22:12:00 -0500

    update manifest

--- src/lib/util/HymnalManifest.ts
+++ src/lib/util/HymnalManifest.ts
@@ -15,7 +15,7 @@ import type { Hymn } from "./types/Hymnal"
 const Hymnal : [Hymn] = [
     {
         title: "Apostles' Creed",
-        index: 1,
+        index: 10,
         license: "Public Domain",
         author: "Unknown",
         slug: "apostles-creed"
@@ -57,18 +57,704 @@ const Hymnal : [Hymn] = [
     },
     {
         title: "Angel Band",
-        index: 16,
+        index: 17,
         license: "Public Domain",
         author: "Jefferson Hascall",
         slug: "angel-band"
     },
     {
         title: "Angels We Have Heard On High",
-        index: 17,
+        index: 18,
         license: "Public Domain",
         author: "Anonymous",
         slug: "angels-we-have-heard-on-high"
-    }
+    },
+    {
+        title: "Are You Washed In The Blood?",
+        index: 19,
+        license: "Public Domain",
+        author: "Elisha Hoffman",
+        slug: "are-you-washed-in-the-blood"
+    },
+    {
+        title: "Away in a Manger",
+        index: 20,
+        license: "Public Domain",
+        author: "Anonymous",
+        slug: "away-in-a-manger"
+    },
+    {
+        title: "Before The Throne",
+        index: 21,
+        license: "Public Domain",
+        author: "Charlie Bancroft",
+        slug: "before-the-throne"
+    },
+    {
+        title: "Be Still My Soul",
+        index: 22,
+        license: "Public Domain",
+        author: "Katherina Von Schlegel",
+        slug: "be-still-my-soul"
+    },
+    {
+        title: "Be Thou My Vision",
+        index: 23,
+        license: "Public Domain",
+        author: "Anonymous",
+        slug: "be-thou-my-vision"
+    },
+    {
+        title: "Blessed Assurance",
+        index: 24,
+        license: "Public Domain",
+        author: "Fanny Crosby",
+        slug: "blessed-assurance"
+    },
+    {
+        title: "Christ The Lord Is Risen Today",
+        index: 25,
+        license: "Public Domain",
+        author: "Charles Wesley",
+        slug: "christ-the-lord-is-risen-today"
+    },
+    {
+        title: "Come If Your're Thirsty",
+        index: 26,
+        license: "Public Domain",
+        author: "Anonymous",
+        slug: "come-if-youre-thristy"
+    },
+    {
+        title: "Come Thou Fount of Every Blessing",
+        index: 27,
+        license: "Public Domain",
+        author: "Robert Robinson",
+        slug: "come-thou-fount-of-every-blessing"
+    },
+    {
+        title: "Come Thou Long-Expected Jesus",
+        index: 28,
+        license: "Public Domain",
+        author: "Charles Wesley",
+        slug: "come-thou-long-expected-jesus"
+    },
+    {
+        title: "Crown Him With Many Crowns",
+        index: 29,
+        license: "Public Domain",
+        author: "Matthew Bridges",
+        slug: "crown-him-with-many-crowns"
+    },
+    {
+        title: "Down By the Riverside",
+        index: 30,
+        license: "Public Domain",
+        author: "Anonymous",
+        slug: "down-by-the-riverside"
+    },
+    {
+        title: "Down to the River to Pray",
+        index: 31,
+        license: "Public Domain",
+        author: "Anonymous",
+        slug: "down-to-the-river-to-pray"
+    },
+    {
+        title: "Doxology",
+        index: 32,
+        license: "Thomas Ken",
+        author: "Anonymous",
+        slug: "doxology"
+    },
+    {
+        title: "Draw Me Nearer",
+        index: 33,
+        license: "Public Domain",
+        author: "Fanny Crosby",
+        slug: "draw-me-nearer"
+    },
+    {
+        title: "Fairest Lord Jesus",
+        index: 34,
+        license: "Public Domain",
+        author: "Anonymous",
+        slug: "fairest-lord-jesus"
+    },
+    {
+        title: "For the Beauty of the Earth",
+        index: 35,
+        license: "Public Domain",
+        author: "Folliott Pierpoint",
+        slug: "for-the-beauty-of-the-earth"
+    },
+    {
+        title: "Go! Tell It on the Mountain",
+        index: 36,
+        license: "Public Domain",
+        author: "Anonymous",
+        slug: "go-tell-it-on-the-mountain"
+    },
+    {
+        title: "God Rest Ye Merry Gentleman",
+        index: 37,
+        license: "Public Domain",
+        author: "Anonymous",
+        slug: "god-rest-ye-merry-gentleman"
+    },
+    {
+        title: "Grace Greater Than Our Sin",
+        index: 38,
+        license: "Public Domain",
+        author: "Julia Johnston",
+        slug: "grace-greater-than-our-sin"
+    },
+    {
+        title: "great-is-thy-faithfulness",
+        index: 39,
+        license: "Public Domain",
+        author: "Thomas Chisholm",
+        slug: "great-is-thy-faithfulness"
+    },
+    {
+        title: "Hard Times",
+        index: 40,
+        license: "Public Domain",
+        author: "Stephen Foster",
+        slug: "hard-times"
+    },
+    {
+        title: "Hark! The Herald Angels Sing",
+        index: 41,
+        license: "Public Domain",
+        author: "Charles Wesley",
+        slug: "hark-the-herald-angels-sing"
+    },
+    {
+        title: "He Is Risen",
+        index: 42,
+        license: "Public Domain",
+        author: "Cecil Frances Alexander",
+        slug: "he-is-risen"
+    },
+    {
+        title: "He Leadeth Me",
+        index: 43,
+        license: "Public Domain",
+        author: "Joseph Gilmore",
+        slug: "he-leadeth-me"
+    },
+    {
+        title: "Here is Love",
+        index: 44,
+        license: "Public Domain",
+        author: "William Res",
+        slug: "here-is-love"
+    },
+    {
+        title: "Holy, Holy, Holy",
+        index: 45,
+        license: "Public Domain",
+        author: "Reginald Heber",
+        slug: "holy-holy-holy"
+    },
+    {
+        title: "How Firm a Foundation",
+        index: 46,
+        license: "Public Domain",
+        author: "John Rippon",
+        slug: "how-firm-a-foundation"
+    },
+    {
+        title: "How Great Thou Art",
+        index: 47,
+        license: "Public Domain",
+        author: "Carl Boberg",
+        slug: "how-great-thou-art"
+    },
+    {
+        title: "I Come to the Garden Alone",
+        index: 48,
+        license: "Public Domain",
+        author: "C. Austin Miles",
+        slug: "i-come-to-the-garden-alone"
+    },
+    {
+        title: "I Have Decided to Follow Jesus",
+        index: 49,
+        license: "Public Domain",
+        author: "Anonymous",
+        slug: "i-have-decided-to-follow-jesus"
+    },
+    {
+        title: "I Heard the Bells on Christmas Day",
+        index: 50,
+        license: "Public Domain",
+        author: "Henry Wadsworth Longfellow",
+        slug: "i-heard-the-bells-on-christmas-day"
+    },
+    {
+        title: "I Love To Tell The Story",
+        index: 51,
+        license: "Public Domain",
+        author: "Arabella Katherine Hankey",
+        slug: "i-love-to-tell-the-story"
+    },
+    {
+        title: "I Need Thee Every Hour",
+        index: 52,
+        license: "Public Domain",
+        author: "Annie Hawks",
+        slug: "i-need-thee-every-hour"
+    },
+    {
+        title: "God Be With You",
+        index: 53,
+        license: "Public Domain",
+        author: "Jeremiah Earnes Rankin",
+        slug: "god-be-with-you"
+    },
+    {
+        title: "I Stand Amazed",
+        index: 54,
+        license: "Public Domain",
+        author: "Charles Gabriel",
+        slug: "i-stand-amazed"
+    },
+    {
+        title: "I Surrender All",
+        index: 55,
+        license: "Public Domain",
+        author: "Winfield S. Weeden",
+        slug: "i-surrender-all"
+    },
+    {
+        title: "In The Sweet By and By",
+        index: 56,
+        license: "Public Domain",
+        author: "Sanford Bennett",
+        slug: "in-the-sweet-by-and-by"
+    },
+    {
+        title: "It Is Well",
+        index: 57,
+        license: "Public Domain",
+        author: "Horatio Spafford",
+        slug: "it-is-well"
+    },
+    {
+        title: "I've Gotta Home in Beulah Land",
+        index: 58,
+        license: "Public Domain",
+        author: "Anonymous",
+        slug: "ive-gotta-home-in-beulah-land"
+    },
+    {
+        title: "Jesus I My Cross Have Taken",
+        index: 59,
+        license: "Public Domain",
+        author: "Henry Lyte",
+        slug: "jesus-i-my-cross-have-taken"
+    },
+    {
+        title: "Jesus Lifted Me",
+        index: 60,
+        license: "Public Domain",
+        author: "Anonymous",
+        slug: "jesus-lifted-me"
+    },
+    {
+        title: "Jesus Loves Me",
+        index: 61,
+        license: "Public Domain",
+        author: "Anna Warner",
+        slug: "jesus-loves-me"
+    },
+    {
+        title: "Jesus Paid It All",
+        index: 62,
+        license: "Public Domain",
+        author: "Elvina Hall",
+        slug: "jesus-paid-it-all"
+    },
+    {
+        title: "Jesus Save Us",
+        index: 63,
+        license: "Public Domain",
+        author: "Anonymous",
+        slug: "jesus-save-us"
+    },
+    {
+        title: "Jesus, What a Friend of Sinners",
+        index: 64,
+        license: "Public Domain",
+        author: "J. Wilbur Chapman",
+        slug: "jesus-what-a-friend-of-sinners"
+    },
+    {
+        title: "Joy to the World",
+        index: 65,
+        license: "Public Domain",
+        author: "Isaac Watts",
+        slug: "joy-to-the-world"
+    },
+    {
+        title: "Joyful, Joyful",
+        index: 66,
+        license: "Public Domain",
+        author: "Henry van Dyke",
+        slug: "joyful-joyful"
+    },
+    {
+        title: "Just a Closer Walk With Thee",
+        index: 67,
+        license: "Public Domain",
+        author: "Anonymous",
+        slug: "just-a-closer-walk-with-thee"
+    },
+    {
+        title: "Leaning on Everlasting Arms",
+        index: 68,
+        license: "Public Domain",
+        author: "Elisha Hoffman",
+        slug: "leaning-on-everlasting-arms"
+    },
+    {
+        title: "Leave It There",
+        index: 69,
+        license: "Public Domain",
+        author: "Charles Tindley",
+        slug: "leave-it-there"
+    },
+    {
+        title: "Love Lifted Me",
+        index: 70,
+        license: "Public Domain",
+        author: "James Roe",
+        slug: "love-lifted-me"
+    },
+    {
+        title: "Man of Sorrows!'",
+        index: 71,
+        license: "Public Domain",
+        author: "Philip Bliss",
+        slug: "man-of-sorrows"
+    },
+    {
+        title: "My Faith Has Found a Resting Place",
+        index: 72,
+        license: "Public Domain",
+        author: "Lidie Edmunds",
+        slug: "my-faith-has-found-a-resting-place"
+    },
+    {
+        title: "My Hope Is Built On Nothing Less",
+        index: 73,
+        license: "Public Domain",
+        author: "Edward Mote",
+        slug: "my-hope-is-built-on-nothing-less"
+    },
+    {
+        title: "My Jesus I Love Thee",
+        index: 74,
+        license: "Public Domain",
+        author: "William Featherston",
+        slug: "my-jesus-i-love-thee"
+    },
+    {
+        title: "Not What My Hands Have Done",
+        index: 75,
+        license: "Public Domain",
+        author: "Horatius Bonar",
+        slug: "not-what-my-hands-have-done"
+    },
+    {
+        title: "Nothing But The Blood",
+        index: 76,
+        license: "Public Domain",
+        author: "Robert Lowry",
+        slug: "nothing-but-the-blood"
+    },
+    {
+        title: "O Come, All Ye Faithful",
+        index: 77,
+        license: "Public Domain",
+        author: "John Wade",
+        slug: "o-come-all-ye-faithful"
+    },
+    {
+        title: "O Come, O Come Emmanuel",
+        index: 78,
+        license: "Public Domain",
+        author: "Anonymous",
+        slug: "o-come-o-come-emmanuel"
+    },
+    {
+        title: "O For a Thousand Tongues to Sing",
+        index: 79,
+        license: "Public Domain",
+        author: "Charles Wesley",
+        slug: "o-for-a-thousand-tongues-to-sing"
+    },
+    {
+        title: "O Holy Night",
+        index: 80,
+        license: "Public Domain",
+        author: "Placide Cappeau",
+        slug: "o-holy-night"
+    },
+    {
+        title: "O Little Town of Bethlehem",
+        index: 81,
+        license: "Public Domain",
+        author: "Phillips Brooks",
+        slug: "o-little-town-of-bethlehem"
+    },
+    {
+        title: "O the Deep, Deep Love of Jesus",
+        index: 82,
+        license: "Public Domain",
+        author: "S. Trevor Frances",
+        slug: "o-the-deep-deep-love-of-jesus"
+    },
+    {
+        title: "O When the Saints",
+        index: 83,
+        license: "Public Domain",
+        author: "Katherine Purvis",
+        slug: "o-when-the-saints"
+    },
+    {
+        title: "O Worship the King",
+        index: 84,
+        license: "Public Domain",
+        author: "Robert Grant",
+        slug: "o-worship-the-king"
+    },
+    {
+        title: "Oh, How I Love Jesus",
+        index: 85,
+        license: "Public Domain",
+        author: "Frederick Whitfield",
+        slug: "oh-how-i-love-jesus"
+    },
+    {
+        title: "Old Rigged Cross",
+        index: 86,
+        license: "Public Domain",
+        author: "George Bernard",
+        slug: "old-rugged-cross"
+    },
+    {
+        title: "On Jordan's Stormy Banks I Stand",
+        index: 87,
+        license: "Public Domain",
+        author: "Samuel Stennett",
+        slug: "on-jordans-stormy-banks-i-stend"
+    },
+    {
+        title: "Peace Like a River",
+        index: 88,
+        license: "Public Domain",
+        author: "Anonymous",
+        slug: "peace-live-a-river"
+    },
+    {
+        title: "Praise to the Lord Almighty",
+        index: 89,
+        license: "Public Domain",
+        author: "Joachim Neander",
+        slug: "praise-to-the-lord-almighty"
+    },
+    {
+        title: "Revive Us Again",
+        index: 90,
+        license: "Public Domain",
+        author: "William Mackay",
+        slug: "revive-us-again"
+    },
+    {
+        title: "Rock of Ages",
+        index: 91,
+        license: "Public Domain",
+        author: "Agustus Toplady",
+        slug: "rock-of-ages"
+    },
+    {
+        title: "Shall We Gather at the River",
+        index: 92,
+        license: "Public Domain",
+        author: "Robert Lowry",
+        slug: "shall-we-gather-at-the-river"
+    },
+    {
+        title: "Silent Night",
+        index: 93,
+        license: "Public Domain",
+        author: "Joseph Mohr",
+        slug: "silent-night"
+    },
+    {
+        title: "Since I Laid My Burdens Down",
+        index: 94,
+        license: "Public Domain",
+        author: "Anonymous",
+        slug: "since-i-laid-my-burdens-down"
+    },
+    {
+        title: "Softly and Tenderly",
+        index: 95,
+        license: "Public Domain",
+        author: "Will Thompson",
+        slug: "softly-and-tenderly"
+    },
+    {
+        title: "Swing Low",
+        index: 96,
+        license: "Public Domain",
+        author: "Anonymous",
+        slug: "swing-low"
+    },
+    {
+        title: "The Welcome Table",
+        index: 97,
+        license: "Public Domain",
+        author: "Anonymous",
+        slug: "the-welcome-table"
+    },
+    {
+        title: "There is a Fountain",
+        index: 98,
+        license: "Public Domain",
+        author: "William Cowper",
+        slug: "there-is-a-fountain"
+    },
+    {
+        title: "There is Power in the Blood",
+        index: 99,
+        license: "Public Domain",
+        author: "Lewis Jones",
+        slug: "there-is-power-in-the-blood"
+    },
+    {
+        title: "This Is My Father's World",
+        index: 100,
+        license: "Public Domain",
+        author: "Maltbie Babcock",
+        slug: "this-is-my-fathers-world"
+    },
+    {
+        title: "This Little Light of Mine",
+        index: 101,
+        license: "Public Domain",
+        author: "Anonymous",
+        slug: "this-little-light-of-mine"
+    },
+    {
+        title: "Tis So Sweet To Trust in Jesus",
+        index: 102,
+        license: "Public Domain",
+        author: "William J. Kirkpatrick",
+        slug: "tis-so-sweet-to-trust-in-jesus"
+    },
+    {
+        title: "To God Be the Glory",
+        index: 103,
+        license: "Public Domain",
+        author: "Fanny Crosby",
+        slug: "to-god-be-the-glory"
+    },
+    {
+        title: "Turn Your Eyes Upon Jesus",
+        index: 104,
+        license: "Public Domain",
+        author: "Helen Lemmel",
+        slug: "turn-your-eyes-upon-jesus"
+    },
+    {
+        title: "Were You There",
+        index: 105,
+        license: "Public Domain",
+        author: "Anonymous",
+        slug: "were-you-there"
+    },
+    {
+        title: "What A Friend We Have In Jesus",
+        index: 106,
+        license: "Public Domain",
+        author: "Joseph Scriven",
+        slug: "what-a-friend-we-have-in-jesus"
+    },
+    {
+        title: "What Child is This?",
+        index: 107,
+        license: "Public Domain",
+        author: "William Dix",
+        slug: "what-child-is-this"
+    },
+    {
+        title: "What Wondrous Love is This?",
+        index: 108,
+        license: "Public Domain",
+        author: "Anonymous",
+        slug: "what-wondrous-love-is-this"
+    },
+    {
+        title: "When I Survey",
+        index: 109,
+        license: "Public Domain",
+        author: "Isaac Watts",
+        slug: "when-i-survey"
+    },
+    {
+        title: "When the Roll",
+        index: 110,
+        license: "Public Domain",
+        author: "James Black",
+        slug: "when-the-roll"
+    },
+    {
+        title: "When We All Get to Heaven",
+        index: 111,
+        license: "Public Domain",
+        author: "Eliza Hewitt",
+        slug: "when-we-all-get-to-heaven"
+    },
+    {
+        title: "Abide With Me",
+        index: 112,
+        license: "Public Domain",
+        author: "Henry Francis Lyte",
+        slug: "abide-with-me"
+    },
+    {
+        title: "Come Thou Almighty King",
+        index: 113,
+        license: "Public Domain",
+        author: "Felice de Giardini",
+        slug: "come-thou-almighty-king"
+    },
+    {
+        title: "O Sacred Head Now Wounded",
+        index: 114,
+        license: "Public Domain",
+        author: "Paul Gerhardt",
+        slug: "o-sacred-head-now-wounded"
+    },
+    {
+        title: "Take My Life",
+        index: 115,
+        license: "Public Domain",
+        author: "Frances Ridley Havergal",
+        slug: "take-my-life"
+    },
+    {
+        title: "The Church's One Foundation",
+        index: 116,
+        license: "Public Domain",
+        author: "Samuel J. Stone",
+        slug: "the-churchs-one-foundation"
+    },
 ]
 
 export default Hymnal
