880b69d3a498e8d980e97d45b5de4206794433dd
Ian Thompson <ianthompson@nicelion.com>
2023-02-19 09:25:37 -0500

    Update styling to footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -11,36 +11,12 @@
 			<h1 class="font-bold text-2xl text-center py-3">HYMNS & HOPS</h1>
 		</nav>
 	</header>
-	<main class="font-body bg-primay-content overflow-y-auto py-4">
+	<main class="font-body bg-primay-content overflow-y-auto py-4 bg-primary-content">
 		<slot />
 	</main>
-	<footer id="footer" class="h-12 bg-black-lighter py-2 flex flex-col text-cornsilk font-body">
-		<div id="copyright" class="text-center space-y-2 text-xs">
-			<div class="text-center">
-				<p>
-					This is an open source project. <a class="underline" target="_blank" href="https://www.github.com/nicelion/hymns">You can contribute here.</a>
-				</p>
-			</div>
-		</div>
+	<footer id="footer" class="bg-black-lighter flex flex-col text-cornsilk font-body">
+		<p class="text-center text-xs py-2">
+			This is an open source project. <a class="underline" target="_blank" href="https://www.github.com/nicelion/hymns">You can contribute here.</a>
+		</p>
 	</footer>
 </div>
-
-<!-- <header class="font-body h-24">
-    <nav class="w-full bg-black-lighter text-cornsilk">
-        <h1 class="font-bold text-2xl text-center py-3">HYMNS & HOPS</h1>
-    </nav>
-</header>
-
-<main class="font-body bg-primay-content h-full overflow-scroll">
-    <slot />
-</main>
-
-<footer id="footer" class="h-12 bg-black-lighter py-2 flex flex-col text-cornsilk font-body">
-	<div id="copyright" class="text-center space-y-2 text-xs">
-		<div class="text-center">
-			<p>
-				Developed by <a class="hover:underline" href="https://www.nicelion.com">Nice Lion Technologies LLC.</a>
-			</p>
-		</div>
-	</div>
-</footer> -->
