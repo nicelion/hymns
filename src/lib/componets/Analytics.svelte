18ca7cc0b0bb64d1f44dbcae44d5d7c51cbbf547
Ian Thompson <ianthompson@nicelion.com>
2023-04-15 14:49:57 -0400

    conditionally render Analytics to prevent dev tracking

--- src/lib/componets/Analytics.svelte
+++ src/lib/componets/Analytics.svelte
@@ -3,6 +3,9 @@
     import { page } from '$app/stores'
 	  import { PUBLIC_MEASUREMENT_ID } from '$env/static/public';
     $: {
+        if (PUBLIC_MEASUREMENT_ID == "") {
+          console.warn("MEASUREMENT_ID not found! In a development environment?")
+        }
         if (typeof gtag !== 'undefined') {
             gtag('config', PUBLIC_MEASUREMENT_ID, {
                 page_title: document.title,
@@ -14,16 +17,17 @@
 </script>
 
 <svelte:head>
-  <script
-    async
-    src={`https://www.googletagmanager.com/gtag/js?id=${PUBLIC_MEASUREMENT_ID}`}>
-  </script>
-  <script>
-    window.dataLayer = window.dataLayer || []
-    function gtag() {
-      dataLayer.push(arguments)
-    }
-    gtag('js', new Date())
-    gtag('config', PUBLIC_MEASUREMENT_ID)
-  </script>
+  {#if PUBLIC_MEASUREMENT_ID != ""}
+    <script async src={`https://www.googletagmanager.com/gtag/js?id=${PUBLIC_MEASUREMENT_ID}`}>
+    </script>
+    <script>
+      window.dataLayer = window.dataLayer || []
+      function gtag() {
+        dataLayer.push(arguments)
+      }
+      gtag('js', new Date())
+      gtag('config', PUBLIC_MEASUREMENT_ID)
+    </script>
+  {/if}
+  
 </svelte:head>
