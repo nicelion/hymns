93e2b3a2a6d1b8012c3042ee03c5ab4c256689a1
Ian Thompson <ianthompson@nicelion.com>
2023-04-23 16:54:48 -0400

    Updated layout to have footer sticky

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,7 +5,7 @@
   
 <Analytics />
 
-<div class="flex flex-col ">
+<!-- <div class="flex flex-col ">
 	<header class="font-body bg-black-lighter sticky top-0">
 		<nav class="w-full  text-cornsilk">
 			<h1 class="font-bold text-2xl text-center py-3">HYMNS & HOPS</h1>
@@ -21,4 +21,24 @@
 			This is an open source project. <a class="underline" target="_blank" href="https://www.github.com/nicelion/hymns">You can contribute here.</a>
 		</p>
 	</footer>
-</div>
+</div> -->
+
+<div class="flex flex-col min-h-screen">
+	<header class="font-body bg-black-lighter sticky top-0">
+		<nav class="w-full  text-cornsilk">
+			<h1 class="font-bold text-2xl text-center py-3">HYMNS & HOPS</h1>
+		</nav>
+	</header>
+	<div class="flex-grow">
+	  <main class="font-body bg-primay-content overflow-y-auto py-4 bg-primary-content flex items-center justify-center">
+		  <div class="w-full max-w-5xl">
+			<slot />
+		  </div>
+	  </main>
+	</div>
+	<footer class="sticky z-50 bg-black-lighter bottom-0 ">
+		<p class="text-center text-xs pb-4 pt-2 text-cornsilk font-body">
+			This is an open source project. <a class="underline" target="_blank" href="https://www.github.com/nicelion/hymns">You can contribute here.</a>
+		</p>
+	</footer>
+  </div>
